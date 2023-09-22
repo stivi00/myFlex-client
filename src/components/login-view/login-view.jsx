6bcda4081c1f1c7aa4878ba4440a8cd78fc82245
stivi00 <stivisth@gmail.com>
2023-09-22 18:46:40 +0200

    mock data for login implemented

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,13 +1,50 @@
-export const LoginView = () => {
+import React from 'react';
+import { useState } from 'react';
+
+export const LoginView = ({ onLoggedIn }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = () => {
+        event.preventDefault();
+
+        const data = {
+            access: username,
+            secret: password,
+        };
+
+        fetch('https://openlibrary.org/account/login.json', {
+            method: 'POST',
+            body: JSON.stringify(data),
+        }).then((response) => {
+            if (response.ok) {
+                onLoggedIn(username);
+            } else {
+                alert('Login failed');
+            }
+        });
+    };
+
+    // usr: 167OLdP5BUfLZGxP
+    // pwd: K39eKYhPMV9DDWhJ
+
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 Username:
-                <input type='text' />
+                <input
+                    type='text'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
             </label>
             <label>
                 Password:
-                <input type='password' />
+                <input
+                    type='password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
             </label>
             <button type='submit'>Submit</button>
         </form>
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -29,7 +29,7 @@ export const MainView = () => {
     }, []);
 
     if (!user) {
-        return <LoginView />;
+        return <LoginView onLoggedIn={(user) => setUser(user)} />;
     }
 
     if (selectedMovie) {
