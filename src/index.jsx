234381c4551d1c804ecd3559a314d61648993222
stivi00 <stivisth@gmail.com>
2023-09-28 12:32:23 +0200

    signup bootstrap form added

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
+import { Container } from 'react-bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 
 const MyFlixApplication = () => {
-    return <MainView className='my-flix' />;
+    return (
+        <Container style={{ border: '1px solid red' }}>
+            <MainView className='my-flix' />
+        </Container>
+    );
 };
 
 const container = document.querySelector('#root');
