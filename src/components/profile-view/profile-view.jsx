aa3c58e1a7469f3f3a36d7e655e368f9d2b9ab76
stivi00 <stivisth@gmail.com>
2023-10-03 16:19:27 +0200

    delete the account added

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
-import { Form, Button, Card, Row, Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Form, Button, Card, Row, Col, Modal } from 'react-bootstrap';
+import { Link, Navigate } from 'react-router-dom';
 
 export const ProfileView = ({ movies }) => {
     const storedUser = JSON.parse(localStorage.getItem('user'));
@@ -9,12 +9,34 @@ export const ProfileView = ({ movies }) => {
     const [user, setUser] = useState(storedUser ? storedUser : null);
     const [token, setToken] = useState(storedToken ? storedToken : null);
 
+    const [showModal, setShowModal] = useState(false);
+
     // this code should work
     const favoriteMovies = movies.filter((m) =>
         user.FavoriteMovies.includes(m.id)
     );
 
+    const handleShowModal = () => setShowModal(true);
+    const handleCloseModal = () => setShowModal(false);
+
     const handleUpdate = () => {};
+
+    const handleDeleteUser = () => {
+        fetch(`https://myflixapi-50hz.onrender.com/users/${user.Username}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }).then((response) => {
+            if (response.ok) {
+                setUser(null);
+
+                alert('Your account has been deleted');
+            } else {
+                alert('something went wrong.');
+            }
+        });
+    };
 
     return (
         <>
@@ -36,6 +58,35 @@ export const ProfileView = ({ movies }) => {
                     </Row>
                 </Col>
             </Row>
+
+            <Row className='justify-content-center'>
+                <Col md={5}>
+                    <Button
+                        variant='link'
+                        className='text-danger'
+                        onClick={handleShowModal}
+                    >
+                        Delete my account
+                    </Button>
+                </Col>
+            </Row>
+
+            <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Delete account</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Are you sure you want to delete your account?
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant='primary' onClick={handleDeleteUser}>
+                        Yes
+                    </Button>
+                    <Button variant='secondary' onClick={handleCloseModal}>
+                        No
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </>
     );
 };
